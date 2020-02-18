d2c6a36aeb87ff37c0283229810ca3e1006ec061
Karran8458 <kgowda@eckerd.edu>
2020-02-18 08:54:28 -0500

    Create GorillaTest.java

---
+++ GorillaTest.java
@@ -0,0 +1,11 @@
+public class GorillaTest extends Mammal {
+	public static void main(String[] args) {
+    Gorilla gorilla1 = new Gorilla(40);
+	gorilla1.eatBananas();
+	gorilla1.eatBananas();
+	gorilla1.throwSomething();
+	gorilla1.throwSomething();
+	gorilla1.throwSomething();
+	gorilla1.climb();
+    }
+}
